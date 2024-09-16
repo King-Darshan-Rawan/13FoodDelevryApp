08c5ec4b3c78c8e6ec781f23fefd4f0f463d4d86
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-09-17 03:17:28 +0530

    navbar update

--- fooddecode/src/comps/Navbar.jsx
+++ fooddecode/src/comps/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/Login");
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <Link className="navbar-brand fs-3 fs-Italic " to="/">
+        <Link className="navbar-brand fs-3 fs-Italic mb-3" to="/">
           Apna Khana
         </Link>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="/navbarNav"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -19,24 +27,31 @@ export const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav me-auto mb-2">
             <li className="nav-item">
-              <Link className={"nav-link active"} aria-current="page" to="/">
+              <Link className={"nav-link active fs-5"} aria-current="page" to="/">
                 Home
-              </Link>
-              {/* <Link className={(isActive)=>{`nav-link active ${isActive ? "text-cyan-800" : "text-cyan-400"}`}} aria-current="page" to="/">Home</Link> */}
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/Login" tabindex="-1">
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link" to="/SingIn" tabindex="-1">
-                SingIn
               </Link>
             </li>
           </ul>
+          <div className="d-flex txt-white m-3">
+            {localStorage.getItem("authToken") ? (
+              <div>
+                <button className="btn bg-white text-success mx-1" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <div>
+                <Link className="btn bg-white text-success mx-1" to="/Login" tabIndex="-1">
+                  Login
+                </Link>
+                <Link className="btn bg-white text-success mx-1" to="/SingIn" tabIndex="-1">
+                  Sign In
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </nav>
